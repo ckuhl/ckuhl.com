79db5d9828d9a344ca22060b68efe2305e0a6e2b
Christian Kuhl <git@christiankuhl.com>
2016-09-07 19:52:28 -0400

    [dev] Create placeholder pages to go with front page

--- personal_site.py
+++ personal_site.py
@@ -5,8 +5,28 @@ app = Flask(__name__)
 
 
 @app.route("/")
-def hello():
-    return render_template("main.html")
+def main():
+    return render_template("home_page.html")
+
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+
+@app.route("/blog")
+def blog():
+    return render_template("blog.html")
+
+
+@app.route("/portfolio")
+def portfolio():
+    return render_template("portfolio.html")
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
