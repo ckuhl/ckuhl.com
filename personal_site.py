868e6f2e413afadcf00f64426b9b5eaffad2ab3c
Christian Kuhl <git@christiankuhl.com>
2017-02-19 21:33:09 -0500

    [dev] Update site style and content

--- personal_site.py
+++ personal_site.py
@@ -2,13 +2,19 @@ from flask import Flask
 from flask import render_template
 from flask_flatpages import FlatPages
 
-# TODO: Set easy way to switch debug state / caching state
-DEBUG = False
+# App constants
+DEBUG = True
 FLATPAGES_AUTO_RELOAD = DEBUG
 FLATPAGES_EXTENSION = '.md'
+FLATPAGES_ROOT = 'static/posts'
+FLATPAGES_MARKDOWN_EXTENSIONS = []
 
+
+# Flask init
 app = Flask(__name__)
 app.config.from_object(__name__)
+
+# Flask-Flatpages init
 pages = FlatPages(app)
 
 
@@ -17,39 +23,44 @@ def main():
     return render_template("pages/home.html")
 
 
-@app.route("/contact")
+@app.route("/contact/")
 def contact():
     return render_template("pages/contact.html")
 
 
-@app.route("/blog")
+@app.route("/blog/")
 def blog():
-    latest = sorted(pages, reverse=True,
-                    key=lambda p: p.meta['date'])
-    return render_template("blog/blog.html", pages=latest)
+    # Articles are pages with a publication date
+    articles = [p for p in pages if 'published' in p.meta
+                and p.meta['published'] is True]
+    latest = sorted(articles, reverse=True,
+                    key=lambda p: p.meta['created'])
+    return render_template("blog/blog.html", articles=latest)
 
 
-@app.route("/portfolio")
+@app.route("/portfolio/")
 def portfolio():
     return render_template("pages/portfolio.html")
 
 
-@app.route("/about")
+@app.route("/about/")
 def about():
     return render_template("pages/about.html")
 
 
 @app.route('/blog/<path:path>/')
 def blog_post(path):
-    page = pages.get_or_404(path)
-    return render_template('blog/blog_post.html', page=page)
+    post = pages.get_or_404(path)
+    return render_template('blog/blog_post.html', post=post)
 
 
-@app.route("/Del")
+# turns out, you can't use `del` as a function name... Oops!
+@app.route("/Del/")
 def danielle():
     return render_template("pages/del.html")
 
 
+# Pass through for Let's Encrypt's certbot to do its thing
 @app.route('/.well-known/acme-challenge/<token_value>')
 def letsencrpyt(token_value):
     with open('.well-known/acme-challenge/{}'.format(token_value)) as f:
@@ -58,4 +69,4 @@ def letsencrpyt(token_value):
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    app.run(debug=DEBUG, host='0.0.0.0')
