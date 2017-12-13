4ddd386f8bf0ce9841a554a7fa236ee804f24a88
Christian Kuhl <git@ckuhl.com>
2017-12-12 19:44:21 -0500

    [fix] Avoid 'a.gif' blacklist

--- ckuhl/analytics.py
+++ ckuhl/analytics.py
@@ -13,12 +13,12 @@ BEACON = base64.b64decode('R0lGODlhAQABAIAAANvf7wAAACH5BAEAAAAALAAAAAABAAEAAAICR
 
 JAVASCRIPT = """(function(){
 var d=document,i=new Image,e=encodeURIComponent;
-i.src='%s/a.gif?url='+e(d.location.href)+'&ref='+e(d.referrer)+'&t='+e(d.title);
+i.src='%s/background.gif?url='+e(d.location.href)+'&ref='+e(d.referrer)+'&t='+e(d.title);
 })()""".replace('\n', '')
 
 analytics = Blueprint('analytics', __name__)
 
-@analytics.route('/a.gif')
+@analytics.route('/background.gif')
 def analyze():
     if not request.args.get('url'):
         abort(404)
@@ -30,7 +30,7 @@ def analyze():
     response.headers['Cache-Control'] = 'private, no-cache'
     return response
 
-@analytics.route('/a.js')
+@analytics.route('/custom.js')
 def script():
     return Response(
             JAVASCRIPT % DOMAIN,
